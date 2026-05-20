6f6f5d39a1b2df8a864df212591ea327cb8203fd
dmytro <dimatsenko@gmail.com>
2026-05-20 13:59:00 +0300

    Implement main view to display 10 random recipes

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,7 @@
+from django.shortcuts import render
+from .models import Recipe
 
+
+def main(request):
+    recipes = Recipe.objects.order_by('?')[:10]
+    return render(request, 'recipe/main.html', {'recipes': recipes})
