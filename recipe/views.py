cde9dac4ac64336ee98babde5ec4df36256eacab
dmytro <dimatsenko@gmail.com>
2026-05-20 14:04:00 +0300

    Implement category_detail view to display recipes of a category

--- recipe/views.py
+++ recipe/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from .models import Recipe
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe, Category
 
 
 def main(request):
     recipes = Recipe.objects.order_by('?')[:10]
     return render(request, 'recipe/main.html', {'recipes': recipes})
+
+
+def category_detail(request, id):
+    category = get_object_or_404(Category, id=id)
+    return render(request, 'recipe/category_detail.html', {'category': category})
